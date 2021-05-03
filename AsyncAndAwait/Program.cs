d4075565f6ef1306ada79db75242a8251e33aef3
Vamshikrishna <Vamshikrishna.Bose@Ltts.com>
2021-05-03 09:12:27 +0530

    Async and await demo

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Factorial;
+using System.Threading;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //async await
+            Method();
+            Console.WriteLine("Main Thread");
+            
+
+            //threading
+            var cal = new CalculateFactorial();
+
+
+            Console.WriteLine("Starting");
+
+            Thread t1 = new Thread(() => cal.Calculate(5));
+            t1.Start();
+            cal.whereToSend += DisplayFactorial;
+            cal.whereToSend += DisplayFactorialMoreThan10k;
+           
+
+            Console.WriteLine("Finished");
+
+
+            Console.ReadLine();
+            
+        }
+
+        //async await
+      public static async void Method()
+        {
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Thread");
+
+        }
+
+       public static void LongTask()
+        {
+            Thread.Sleep(2000);
+        }
+
+
+        //threading
+
+       public static void DisplayFactorial(BigInteger value)
+        {
+            Console.WriteLine(value);
+        }
+       public static void DisplayFactorialMoreThan10k(BigInteger value)
+        {
+            if (value > 10000)
+            {
+                Console.WriteLine("Big value");
+            }
+            else
+            {
+                Console.WriteLine("Small value");
+            }
+        }
+
+    }
+}
---
+++ Factorial/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    public class CalculateFactorial
+    {
+
+        public delegate void sendResult(BigInteger num);
+        public event sendResult whereToSend;
+        //5 => 5*4*3*2*1
+        public  void Calculate(BigInteger num)
+        {
+            BigInteger result = num;
+
+            for (BigInteger i =1; i<num; i++)
+            {
+                result = result * i;
+                
+            }
+            whereToSend(result);
+        }
+    }
+}
